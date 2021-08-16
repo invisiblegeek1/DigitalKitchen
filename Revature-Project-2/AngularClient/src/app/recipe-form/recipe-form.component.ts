305682784c0bd8e277158e82637018d09859c2df
Ragul Arulanadam <srsivaragul@gmail.com>
2021-08-16 17:58:13 +0530

    Modified

--- Revature-Project-2/AngularClient/src/app/recipe-form/recipe-form.component.ts
+++ Revature-Project-2/AngularClient/src/app/recipe-form/recipe-form.component.ts
@@ -10,70 +10,73 @@ import {FormArray, FormControl, FormGroup,Validators} from '@angular/forms';
   styleUrls: ['./recipe-form.component.css']
 })
 export class RecipeFormComponent  {
-  // angForm = new FormGroup({
-  //   names: new FormArray([
-  //    this.ingredients()
-  //   ]),
-  //   // amount : new FormArray([
-  //   //   new FormControl('', Validators.required),
-  //   //   new FormControl('', Validators.required),
-
-  //   // ])
-  // });
-  // ingredients(): FormGroup{
-  //   return new FormGroup({
-  //     name : new FormControl('', Validators.required),
-  //     amount : new FormControl('', Validators.required),
-  //   })
-  // }
-  // get names(): FormArray {
-  //    return this.angForm.get('names') as FormArray;
-     
-  // }
-  // get amount(): FormArray{
-  //   return this.angForm.get('amount') as FormArray;
-  // }
-  // onFormSubmit(): void {
-  //   for (let i = 0; i < this.names.length; i++) {
-  //     console.log(this.names.at(i).value);
-  //   }
-    
-  // }
-  // addNameField() {
-  //   this.names.push(new FormControl('', Validators.required));
-
-  // }
-
-  // deleteNameField(index: number) {
-  //   if (this.names.length !== 1) {
-  //     this.names.removeAt(index);
-  //   }
-  //   console.log(this.names.length);
-  // }
-  data:Array<any> = [] 
-  AddData: Array<any> = []
-
-
-
-  constructor(private recipeForm: RecipeFormService,private http:HttpClient) { }
-
-  ngOnInit(): void {
+  angForm = new FormGroup({
+    names: new FormArray([
+    //  this.ingredients()
+    new FormControl('', Validators.required),
+    ]),
+    amount: new FormArray([
+      new FormControl('',Validators.required)
+    ])
+  
+  });
+  ingredients(): FormGroup{
+    return new FormGroup({
+      name : new FormControl('', Validators.required),
+      amount : new FormControl('', Validators.required),
+    })
   }
+  get names(): FormArray {
+     return this.angForm.get('names') as FormArray;
+     
+  }
+  get amount(): FormArray{
+    return this.angForm.get('amount') as FormArray;
+  }
+  onFormSubmit(): void {
+    for (let i = 0; i < this.names.length; i++) {
+      console.log(this.names.at(i).value);
+    }
+    for (let i = 0; i < this.amount.length; i++) {
+      console.log(this.amount.at(i).value);
+    }
+    
+  }
+  addNameField() {
+    this.names.push(new FormControl('', Validators.required));
+    this.amount.push(new FormControl('', Validators.required));
+  }
+
+  deleteNameField(index: number) {
+    if (this.names.length !== 1) {
+      this.names.removeAt(index);
+    }
+    console.log(this.names.length);
+  }
+  // data:Array<any> = [] 
+  // AddData: Array<any> = []
+
+
+
+  // constructor(private recipeForm: RecipeFormService,private http:HttpClient) { }
+
+  // ngOnInit(): void {
+  // }
   
 
-  OnSubmit(data:any){
-    this.data = [...this.data,...this.AddData]
-    console.log(data)   
-    // return this.recipeForm.getRecipeData(data);
-  }
+  // OnSubmit(data:any){
+  //   this.data = [...this.data,...this.AddData]
+  //   console.log(data)   
+  //   // return this.recipeForm.getRecipeData(data);
+  // }
 
 
 
-  addData(){
-    let Name = document.getElementById("Ingredient_Name");
-    let Amount =document.getElementById("Ingredient_Amount")
-    this.AddData = [...this.AddData,...[Name,Amount]]
-    console.log(this.AddData)
+  // addData(){
+  //   let Name = document.getElementById("Ingredient_Name");
+  //   let Amount =document.getElementById("Ingredient_Amount")
+  //   this.AddData = [...this.AddData,...[Name,Amount]]
+  //   console.log(this.AddData)
 
-  }
+  // }
 }
